d2be86a207023c9a4c747a2ef2922132cd26eee2
Tanishq Arora <tanishqarora@microsoft.com>
2024-12-20 11:10:28 +0530

    k

--- .script/tests/solutionPackageValidation/solutionPackageUIValidation.py
+++ .script/tests/solutionPackageValidation/solutionPackageUIValidation.py
@@ -725,7 +725,7 @@ def main():
     solutionPackage = extractInfo(evaluatedTemplates, mainTemplateFilePath, createUiDefinitionFilePath)
 
     print(solutionPackage)
-    
+
     with open(".\\templates\\solution.json", 'w', encoding='utf-8') as file:
         json.dump(solutionPackage, file, indent=4)
         file.close()
@@ -771,39 +771,5 @@ if __name__ == '__main__':
     main()
     print("Done")
 
-def main():
-    modifiedFiles = getModifiedFiles()
-    with open("modifiedFiles.txt", "w") as file:
-        for modifiedFile in modifiedFiles:
-            file.write(modifiedFile + "\n")
-        file.close()
-
-def getModifiedFiles():
-    gitRemoteCommand = "git remote"
-    remoteResult = subprocess.run(gitRemoteCommand, shell=True, text = True, capture_output=True, check=True)
-    
-    if "origin" not in remoteResult.stdout.split():
-        gitAddoriginCommand = f"git remote add origin {RepoUrl}"
-        subprocess.run(gitAddoriginCommand, shell=True, text = True, capture_output=True, check=True)
-
-    gitFetchOrigin = "git fetch origin master"
-    
-    subprocess.run(gitFetchOrigin, shell=True, text = True, capture_output=True, check=True)
-    
-
-    gitDiffCommand = "git diff origin/master --name-only"
-
-    diffResult = subprocess.run(gitDiffCommand, shell=True, text = True, capture_output=True, check=True)
-    
-    modifiedFiles = [file for file in diffResult.stdout.split('\n') if re.match(r"Solutions/.*/Package/mainTemplate.json", file) or re.match(r"Solutions/.*/Package/createUiDefinition.json", file)]
-
-    # for file in diffResult.stdout.split('\n'):
-    #     if re.match(r"Solutions/.*/Package/mainTemplate.json", file) or re.match(r"Solutions/.*/Package/createUiDefinition.json", file):
-    
-    return modifiedFiles
-
-if __name__ == "__main__":
-    load_dotenv()
-    main()
 
 
