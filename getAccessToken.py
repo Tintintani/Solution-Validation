c776b434803b4e06c01b51c08b46314da3d28c5b
Tanishq Arora <tanishqarora@microsoft.com>
2025-01-16 15:08:21 +0530

    Untracked redundant files and added AWS

--- getAccessToken.py
+++
@@ -1,7 +0,0 @@
-import json
-import re
-import subprocess
-
-result = subprocess.run('az account get-access-token --query accessToken --output json', capture_output=True, shell=True)
-result = json.loads(result.stdout)
-print(result)
